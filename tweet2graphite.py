40dee9cb7a313ddb4367887339b215d32c7c1dfc
Denis Zhdanov <denis.zhdanov@gmail.com>
2013-08-25 22:28:56 +0200

    Using marvelous Pattern module for searching and processing

--- tweet2graphite.py
+++ tweet2graphite.py
@@ -1,11 +1,20 @@
 import socket
+import time
+import calendar
 from pattern.web import Twitter, plaintext
-from pattern.nl  import sentiment as sentiment_nl
-from pattern.en  import sentiment as sentiment_en
-from pattern.fr  import sentiment as sentiment_fr
+from pattern.nl import sentiment as sentiment_nl
+from pattern.en import sentiment as sentiment_en
+from pattern.fr import sentiment as sentiment_fr
 
 CARBON_SERVER = '127.0.0.1'
 CARBON_PORT = 2003
+CARBON_METRIC = 'mp.events.twitter'
+
+
+def datetime_to_ts(datestring):
+    t = time.strptime(datestring, '%a %b %d %H:%M:%S +0000 %Y')
+    return calendar.timegm(t)
+
 
 def text_sentiment(tweet):
     polarity = 0
@@ -18,7 +27,12 @@ def text_sentiment(tweet):
             (polarity, subjectivity) = sentiment_en(s)
         if tweet.language == "fr":
             (polarity, subjectivity) = sentiment_fr(s)
+        if ':)' in s:
+            polarity = 1
+        if ':(' in s:
+            polarity = -1
     return polarity*(1.0 - subjectivity)
+
 
 def send_msg_to_graphite(metric, value, timestamp):
     message = '%s %s %d\n' % (metric, value, timestamp)
@@ -28,8 +42,11 @@ def send_msg_to_graphite(metric, value, timestamp):
     sock.close()
 
 term = 'marktplaats'
-for tweet in Twitter().search(term):
+start = None
+count = 100
+for tweet in Twitter().search(term, start=start, count=count):
     text = plaintext(tweet.description)
     if term in text: # skip non-relevant results
         weight = 1 + text_sentiment(tweet)
         print tweet.date, text, weight
+        print datetime_to_ts(tweet.date), weight
