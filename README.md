2035f7a9431b7191ba2520178a02ff64fd05743e
Denis Zhdanov <denis.zhdanov@gmail.com>
2013-08-29 17:35:20 +0200

    Fully working now!

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 tweet2graphite
 ==============
 
-Simple script which will search tweets, calculate words sentiment and put results to predefined graphite metric
+Simple script which will search tweets for specific term, calculate words sentiment and put results to predefined graphite metric
+
+Tweet weight will be calculated as zero_weight +/- tweet sentiment
+Tweet sentiment is calculated as polarity*(1.0 - subjectivity)
+polarity and subjectivity calculated by magnificent (Pattern module|http://www.clips.ua.ac.be/pages/pattern) for Dutch and English tweets
+1 < polarity < -1
+0 < subjectivity < 1
+Last tweet timestamp will be put in last_ts_file (yep, I know, it will work only for small amount of tweets, maybe I'll create tweet stream version of this if needed)
+Do not run this script too often - or Twitter will block you (150 req for 15 min MAX for now)
+
+For config see config.ini
+Put it in ~/.t2g/config.ini
+
+zero_weight = 1
+do_sentiments = True
+search_term = marktplaats
+results_count = 100
+carbon_server = 127.0.0.1
+carbon_port = 2003
+carbon_metric = mp.events.twitter
+last_ts_file = ~/.t2g/last_ts
--- tweet2graphite.py
+++ tweet2graphite.py
@@ -1,15 +1,14 @@
-import socket
 import time
 import calendar
+import os
+import sys
+import ConfigParser
+from socket import socket
 from pattern.web import Twitter, plaintext
 from pattern.nl import sentiment as sentiment_nl
 from pattern.en import sentiment as sentiment_en
-from pattern.fr import sentiment as sentiment_fr
 
-CARBON_SERVER = '127.0.0.1'
-CARBON_PORT = 2003
-CARBON_METRIC = 'mp.events.twitter'
-
+CONFIG_FILE = '~/.t2g/config.ini'
 
 def datetime_to_ts(datestring):
     t = time.strptime(datestring, '%a %b %d %H:%M:%S +0000 %Y')
@@ -25,8 +24,6 @@ def text_sentiment(tweet):
             (polarity, subjectivity) = sentiment_nl(s)
         if tweet.language == "en":
             (polarity, subjectivity) = sentiment_en(s)
-        if tweet.language == "fr":
-            (polarity, subjectivity) = sentiment_fr(s)
         if ':)' in s:
             polarity = 1
         if ':(' in s:
@@ -34,19 +31,82 @@ def text_sentiment(tweet):
     return polarity*(1.0 - subjectivity)
 
 
-def send_msg_to_graphite(metric, value, timestamp):
+def send_msg_to_graphite(CARBON_SERVER, CARBON_PORT, metric, value, timestamp):
+    sock = socket()
+    try:
+        sock.connect((CARBON_SERVER, CARBON_PORT))
+    except:
+        print("Couldn't connect to Graphite, is carbon-relay running?")
+        return
     message = '%s %s %d\n' % (metric, value, timestamp)
-    sock = socket.socket()
-    sock.connect(CARBON_SERVER, CARBON_PORT)
-    sock.sendall(message)
-    sock.close()
+    try:
+        sock.sendall(message)
+        sock.close()
+    except Exception as out:
+        print("Updating graphite failed: %s" % out)
+        return
 
-term = 'marktplaats'
-start = None
-count = 100
-for tweet in Twitter().search(term, start=start, count=count):
-    text = plaintext(tweet.description)
-    if term in text: # skip non-relevant results
-        weight = 1 + text_sentiment(tweet)
-        print tweet.date, text, weight
-        print datetime_to_ts(tweet.date), weight
+
+if __name__ == "__main__":
+    # read config
+    search_term = None
+    start = None
+    results_count = 100
+    CARBON_SERVER = '127.0.0.1'
+    CARBON_PORT = 2003
+    CARBON_METRIC = 'mp.events.twitter'
+    weight = 1
+    do_sentiments = True
+    last_ts = 0
+
+    try:
+        config = ConfigParser.RawConfigParser()
+        config.readfp(open(os.path.expanduser(CONFIG_FILE)))
+
+        search_term = config.get("tweet2graphite", "search_term")
+        CARBON_SERVER = config.get("tweet2graphite", "carbon_server")
+        CARBON_PORT = config.get("tweet2graphite", "carbon_port")
+        CARBON_METRIC = config.get("tweet2graphite", "carbon_metric")
+        last_ts_file = config.get("tweet2graphite", "last_ts_file")
+        results_count = config.get("tweet2graphite", "results_count")
+        zero_weight = config.get("tweet2graphite", "zero_weight")
+        do_sentiments = config.get("tweet2graphite", "do_sentiments")
+    except IOError, e:
+        print "Config %s not found" % CONFIG_FILE
+        sys.exit(1)
+
+    # read last start position from file
+    try:
+        last_ts_file = os.path.expanduser(last_ts_file)
+        with open(last_ts_file, "r") as myfile:
+            last_ts = int(myfile.read().replace('\n', ''))
+        myfile.close()
+    except IOError, e:
+        pass
+    #print "Last TS=", last_ts
+
+    print search_term, start, results_count
+    max_ts = last_ts
+    for tweet in Twitter().search(search_term, start=start, count=results_count):
+        text = plaintext(tweet.description)
+        ts = datetime_to_ts(tweet.date)
+        print ts, text
+        if ts >= last_ts:  # skip old results
+            weight = float(zero_weight)
+            if do_sentiments:
+                weight = weight + text_sentiment(tweet)
+
+            #print tweet.date, text, weight
+            #print ts, weight
+            send_msg_to_graphite(CARBON_SERVER, CARBON_PORT, CARBON_METRIC, weight, ts)
+            if ts > max_ts:
+                max_ts = ts
+
+    # save last_ts to file
+    try:
+        with open(last_ts_file, "w+") as myfile:
+            print >> myfile, max_ts
+        myfile.close()
+    except IOError, e:
+        print "Can't save last_ts to file %s" % last_ts_file
+        sys.exit(1)
